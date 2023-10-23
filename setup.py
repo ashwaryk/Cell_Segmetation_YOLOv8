bac04b4d23942dd508fee639ea3dbadf75c765f8
ashwaryk <ashwarykaushik@gmail.com>
2023-10-23 16:46:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "cellSegmentation",
+    version = "0.0.0",
+    author = "Ashwary Kaushik",
+    author_email = "ashwarykaushik@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "cellsegmentation"
+project_name = "cellSegmentation"
 
 list_of_files = [
     ".github/workflows/.getkeep",
