654d6e76598899f3a58246ef7bb72de23e6324e2
ashwaryk <ashwarykaushik@gmail.com>
2023-10-25 14:52:11 +0530

    Data ingestion done

--- cellsegmentation/constant/training_pipeline/__init__.py
+++ cellsegmentation/constant/training_pipeline/__init__.py
@@ -8,3 +8,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/17vDLJrk4iYtysocirKNCDuDIOAsX0-17/view?usp=sharing"
+
+
+
+"""
+Data validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE: str = "status.txt"
+
+DATA_VALIDATION_ALL_REQUIRED_FILES: str = ["train", "valid", "test", "data.yaml"]
